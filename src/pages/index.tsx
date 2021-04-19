e12a29b1ef29f5be9da801e4a16099568ad9d15a
jonashenrique90 <jonas.herique90@gmail.com>
2021-04-19 12:41:14 -0300

    add initial config

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SSR styled-components with Next.js Starter</title>
+      </Head>
+      <h1>Hello, world!</h1>
+    </>
+  );
+}
